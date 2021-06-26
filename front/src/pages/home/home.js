332f26cf55cef2006dd184b9cc173cb51d149269
ojuliomiguel <juliomiguelsouzacosta@gmail.com>
2021-06-26 12:27:51 -0300

    feat: cria home page

---
+++ front/src/pages/home/home.js
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <div className="w3-container">
+            <h2 className="w3-cursive w3-display-middle" >Manga Rosa LTDA</h2>
+        </div>
+    )
+}
+
+export default Home;
