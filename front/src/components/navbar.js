956980551601e03a625276fac8229327ba33d53f
ojuliomiguel <juliomiguelsouzacosta@gmail.com>
2021-06-26 21:04:43 -0300

    style: altera cor do NavBar

--- front/src/components/navbar.js
+++ front/src/components/navbar.js
@@ -1,13 +1,17 @@
 import { NavLink } from 'react-router-dom'
 const Navbar = () => {
     return (
-        <nav className="w3-bar w3-pink">
+        <nav className="w3-bar w3-blue">
             <NavLink className="w3-bar-item w3-button" to="/" exact>
                 Home
             </NavLink>
             
             <NavLink className="w3-bar-item w3-button" to="/" exact>
                 Registros
+            </NavLink>
+
+            <NavLink className="w3-bar-item w3-button" to="/" exact>
+                Validar
             </NavLink>
         </nav>
     )
