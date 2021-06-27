dcd26b4d1a0f63e681f7847b9236ec5c4056e66b
ojuliomiguel <juliomiguelsouzacosta@gmail.com>
2021-06-26 21:27:58 -0300

    feat: adiciona link ao navbar

--- front/src/components/navbar.js
+++ front/src/components/navbar.js
@@ -2,11 +2,11 @@ import { NavLink } from 'react-router-dom'
 const Navbar = () => {
     return (
         <nav className="w3-bar w3-blue">
-            <NavLink className="w3-bar-item w3-button" to="/" exact>
+            <NavLink className="w3-bar-item w3-button" to="" exact>
                 Home
             </NavLink>
             
-            <NavLink className="w3-bar-item w3-button" to="/" exact>
+            <NavLink className="w3-bar-item w3-button" to="/registrar" exact>
                 Registros
             </NavLink>
 
